273ab7a34352ecf558054a9df1b9f38242332773
DilmyPerera <samadiniperera20@gmail.com>
2024-11-24 09:05:40 +0530

    ✨feature: Add alignments to input fields and table cells in Calculator component

--- calculator/src/components/Calculator/Calculator.js
+++ calculator/src/components/Calculator/Calculator.js
@@ -4,37 +4,37 @@ export default function Calculator(props){
     const [num1, setNum1] = useState(0);
     const [num2, setnum2] = useState(0);
     return(
-        <div>
+        <div className="operation">
             <table>
                 <tbody>
-                    <tr>
+                    <tr align="right">
                         <td>Enter Number 1: </td>
                         <td>
                             <input type="number" value={num1} 
                             onChange = {(e)=>{setNum1(e.target.value)}}/>
                         </td>
                     </tr>
-                    <tr>
+                    <tr align="right">
                         <td>Enter Number 2 : </td>
                         <td><input type="number" value={num2}
                         onChange = {(e)=>{setnum2(e.target.value)}}/>
                         </td>
                     </tr>
                     <tr>
-                        <td>Addition : </td>
-                        <td>{Number(num1)+Number(num2)}</td>
+                        <td align="right">Addition : </td>
+                        <td align="left">{Number(num1)+Number(num2)}</td>
                     </tr>
                     <tr>
-                        <td>Subtraction</td>
-                        <td>{num1-num2}</td>
+                        <td align="right">Subtraction : </td>
+                        <td align="left">{num1-num2}</td>
                     </tr>
                     <tr>
-                        <td>Multiplicatoin</td>
-                        <td>{num1*num2}</td>
+                        <td align="right">Multiplicatoin : </td>
+                        <td align="left">{num1*num2}</td>
                     </tr>
                     <tr>
-                        <td>Division</td>
-                        <td>{num1*num2}</td>
+                        <td align="right">Division : </td>
+                        <td align="left">{num1*num2}</td>
                     </tr>
                 </tbody>
             </table>
