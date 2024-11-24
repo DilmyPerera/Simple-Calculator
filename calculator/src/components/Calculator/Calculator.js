5fc7a216c7010788b1d8ec2a5117c8c00515963d
DilmyPerera <samadiniperera20@gmail.com>
2024-11-24 08:37:19 +0530

    ✨feature: Add table to Calculator component for basic arithmetic operations

--- calculator/src/components/Calculator/Calculator.js
+++ calculator/src/components/Calculator/Calculator.js
@@ -1,7 +1,34 @@
 export default function Calculator(props){
     return(
         <div>
-            
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Enter Number 1: </td>
+                        <td><input type="number" value={num1}/> </td>
+                    </tr>
+                    <tr>
+                        <td>Enter Number 2 : </td>
+                        <td><input type="number" value={num2}/></td>
+                    </tr>
+                    <tr>
+                        <td>Addition : </td>
+                        <td>{Number(num1)+Number(num2)}</td>
+                    </tr>
+                    <tr>
+                        <td>Subtraction</td>
+                        <td>{num1-num2}</td>
+                    </tr>
+                    <tr>
+                        <td>Multiplicatoin</td>
+                        <td>{num1*num2}</td>
+                    </tr>
+                    <tr>
+                        <td>Division</td>
+                        <td>{num1*num2}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
